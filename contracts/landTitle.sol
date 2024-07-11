55009c168627c9255719ba4378bd1bd94f69b3d7
HarshWagh243 <harshwagh289@gmail.com>
2024-07-11 19:10:34 +1000

    Added functions in user and landtitle

--- contracts/landTitle.sol
+++ contracts/landTitle.sol
@@ -1,34 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 // To - DO
-// add a mapping to map title id to owner id
+// add a mapping to  map title id to owner id
 // probably have an escrow contract to manage transaction
 // figure out more functionalities and add them in to-do
+
+
+// not sure if ownable should be used
 contract LandTitle is Ownable {
-    struct Title {
-        uint256 id;
-        string details;
+    address public oracle;      // Oracle address
+    uint256 public timeout;     // Timeout in AEST as UNIX timestamp
+
+    struct Title{
+        uint256 id; // unique identifier
+        string details; // location details
         address owner;
         uint256 price;
         bool forSale;
     }
 
-    mapping(uint256 => Title) public titles;
+    mapping(uint256 => Title) public titles; // map title_id to Title
+    mapping(uint256 => address) public owners; // map title_id to owner
     uint256 public nextId;
 
+    // Events informing contract activities
+    event checkOwnership(address owner, uint256 titleId);
     event TitleCreated(uint256 id, string details, address owner);
     event TitleForSale(uint256 id, uint256 price);
     event TitleSold(uint256 id, address newOwner);
 
-    constructor(address oracleAddress) {
-        oracle = PaymentOracle(oracleAddress);
+    /**
+     * @dev Constructor.
+     *
+     * @param _oracle Oracle address 
+     * @param _timeout Timeout in AEST as UNIX timestamp 
+     */
+    constructor(address _oracle, uint256 _timeout) {
+        oracle = _oracle;
+        timeout = _timeout;
     }
 
+    /**
+     * @dev Request to create title as a owner. Once the request is made
+     * oracle is informed to perform verification and send status via event
+     */
     function createTitle(string memory _details) public onlyOwner {
         titles[nextId] = Title(nextId, _details, msg.sender, 0, false);
+        owners[nextId] = msg.sender;
         emit TitleCreated(nextId, _details, msg.sender);
         nextId++;
     }
@@ -56,63 +77,3 @@ contract LandTitle is Ownable {
         emit TitleSold(_id, msg.sender);
     }
 }
-
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "../node_modules/@openzeppelin";
-
-// To - DO
-// add a mapping to map title id to owner id
-// probably have an escrow contract to manage transaction
-// figure out more functionalities and add them in to-do
-contract LandTitle is Ownable {
-    struct Title {
-        uint256 id;
-        string details;
-        address owner;
-        uint256 price;
-        bool forSale;
-    }
-
-    mapping(uint256 => Title) public titles;
-    uint256 public nextId;
-
-    event TitleCreated(uint256 id, string details, address owner);
-    event TitleForSale(uint256 id, uint256 price);
-    event TitleSold(uint256 id, address newOwner);
-
-    constructor(address oracleAddress) {
-        oracle = PaymentOracle(oracleAddress);
-    }
-
-    function createTitle(string memory _details) public onlyOwner {
-        titles[nextId] = Title(nextId, _details, msg.sender, 0, false);
-        emit TitleCreated(nextId, _details, msg.sender);
-        nextId++;
-    }
-
-    function getTitle(uint256 _id) public view returns (Title memory) {
-        return titles[_id];
-    }
-
-    function putTitleForSale(uint256 _id, uint256 _price) public {
-        require(titles[_id].owner == msg.sender, "Only the owner can put the title for sale");
-        titles[_id].price = _price;
-        titles[_id].forSale = true;
-        emit TitleForSale(_id, _price);
-    }
-
-    function buyTitle(uint256 _id) public {
-        require(titles[_id].forSale, "This title is not for sale");
-        require(oracle.verifyPayment(msg.sender, titles[_id].price), "Payment not verified");
-
-        address seller = titles[_id].owner;
-        titles[_id].owner = msg.sender;
-        titles[_id].forSale = false;
-        titles[_id].price = 0;
-
-        emit TitleSold(_id, msg.sender);
-    }
-}
-
--- contracts/user.sol
+++ contracts/user.sol
@@ -1,7 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./landTitle.sol";
+// Steps
+// register user ask oracle for permission
+// if the user wants to add their land to owns
+    // ask oracle for verification and add land to their owns map (this could be integrated with the landtitle)
+    // use addtitle from landtitle
+// if the user wants to buys a land
+    // initiate transaction using buytitle in landtitle
+    // verification of transaction
+    // change of ownership
+// if user wants to sell a land
+    // call putforsale in landtitle   
+
 // To -do 
+// oracle to database connectivity(at the very end if possible)
+// 
 // figure out more user attributes (other than ID)
 // 
 // This will import landTitle contract
@@ -11,9 +26,44 @@ pragma solidity ^0.8.0;
                         // OR
     // should we have a list for each user to store the land that they own (probably because a user can be either buyer 
     // or seller, in the later stages a buyer(intially had no land) may want to sell the land they own)a
-contract userFunctionality{
+contract userFunctionality is landTitle{
+    LandTitle public titleContract;
+    LandTitle public titleContAddress;
+
+
+    mapping(address => userDetails) users;
+
     struct userDetails{
         address userId;
-        mapping(uint256 => bool) owns; //map title id 
+        uint256 certificateId; //stores id of certificate issued by CA
+        mapping(uint256 => bool) owns; //map title id
+    }
+
+    /**
+     * @dev Constructor.
+     *
+     * @param _landTitleAddress LandTitle contract address 
+     */
+    constructor(address _landTitleAddress){
+        titleContract = LandTitle();
+        titleContAddress = LandTitle(_landTitleAddress);
+    }
+
+    /**
+     * @dev add users to the network.
+     */
+    function addUsers() public{
+        // get userId and certificateId from oracle
+        // users[msg.sender] = 
+    }
+
+    /**
+     * @dev add users owned land.
+     *
+     * @param _title LandTitle details (struct from landtitle) 
+     */
+    function addLandDetails(string memory details) public{
+        // oracle will verify the details and then 
+        titleContAddress.createTitle(details);
     }
 }
